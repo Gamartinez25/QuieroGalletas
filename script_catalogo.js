85cf5ba16010f18e0cd799d7fa14d535b96b89de
Gamartinez25 <gamartinez.21@ucab.est.edu.ve>
2025-07-31 02:02:51 -0400

    version 2.1

--- script_catalogo.js
+++ script_catalogo.js
@@ -31,3 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
             productGrid.innerHTML = '<p>Lo sentimos, no pudimos cargar los productos en este momento.</p>';
         });
 });
+document.addEventListener('DOMContentLoaded', function() {
+            const hamburgerMenu = document.querySelector('.hamburger-menu');
+            const navLinks = document.querySelector('.nav-links');
+
+            hamburgerMenu.addEventListener('click', function() {
+                navLinks.classList.toggle('active');
+                hamburgerMenu.classList.toggle('open');
+            });
+
+            // Optional: Close menu when a link is clicked
+            navLinks.querySelectorAll('a').forEach(link => {
+                link.addEventListener('click', () => {
+                    if (navLinks.classList.contains('active')) {
+                        navLinks.classList.remove('active');
+                        hamburgerMenu.classList.remove('open');
+                    }
+                });
+            });
+        });
